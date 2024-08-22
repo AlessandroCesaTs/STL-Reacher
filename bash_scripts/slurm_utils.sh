7d462e4b556e788d5a288348e5a0dbd4d8511032
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2024-08-22 18:41:20 +0200

    corrected slurm_utils.sh

--- bash_scripts/slurm_utils.sh
+++ bash_scripts/slurm_utils.sh
@@ -2,22 +2,22 @@
 
 # Function to run a command either with or without srun based on if it's in a slurm environment
 srun_if_on_slurm() {
-    if [ -z "$SLURM_JOB_ID" ]; then
+    if command -v srun &> /dev/null; then
+        # Running on SLURM 
+        srun $@
+    else
         # Not running on SLURM (likely running locally)
         $@
-    else
-        # Running on SLURM
-        srun $@
     fi
 }
 
 # Function to run a command either with bash or with sbatch based on if it's in a slurm environment
 bash_or_sbatch() {
-    if [ -z "$SLURM_JOB_ID" ]; then
+    if command -v sbatch &> /dev/null; then
+        # Running on SLURM 
+        sbatch $@
+    else
         # Not running on SLURM (likely running locally)
         bash $@
-    else
-        # Running on SLURM
-        sbatch $@
     fi
 }
