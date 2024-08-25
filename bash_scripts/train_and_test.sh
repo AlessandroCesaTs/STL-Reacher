c7954baa2c669ff35580babe2719c732f8dac9cd
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2024-08-25 15:20:12 +0200

    typo fix

--- bash_scripts/train_and_test.sh
+++ bash_scripts/train_and_test.sh
@@ -3,7 +3,7 @@
 if command -v sbatch &> /dev/null; then
     #I'm on slurm
     train_job_id=$(sbatch bash_scripts/train.sh | awk '{print $4}')
-    test_job_id=$(sbatch --dependency=afterok:$train_job_id bash_scripts/tesr.sh | awk '{print $4}')
+    test_job_id=$(sbatch --dependency=afterok:$train_job_id bash_scripts/test.sh | awk '{print $4}')
     echo "Submitted jobs $train_job_id, $test_job_id"
 else
     bash_scripts/train.sh
