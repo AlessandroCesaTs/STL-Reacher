9760e87acf9a8f2cf1f279a171db32ff30fd4c52
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2024-10-05 17:11:43 +0200

    training with new monitoring

--- bash_scripts/train.sh
+++ bash_scripts/train.sh
@@ -17,8 +17,8 @@ num_of_iters=${2:-1}
 source bash_scripts/slurm_utils.sh  #get slurm utils functions
 
 if i_am_on_slurm; then
-    total_timesteps=1536000
-    n_steps=2048
+    total_timesteps=153600
+    n_steps=2000
     n_epochs=10
     max_steps=1000
 else
