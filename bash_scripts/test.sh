949653dc701d4aa755727eb33d9b7d1009303962
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2024-10-11 23:41:07 +0200

    running double

--- bash_scripts/test.sh
+++ bash_scripts/test.sh
@@ -10,7 +10,7 @@
 #SBATCH --no-requeue
 #SBATCH --get-user-env
 
-change_target=${1:-'--change-target'}
+change_target=${1:-'--double'}
 hard_reward=${2:-'--hard_reward'}
 output_path=$(pwd)/outputs/${3:-output}
 
@@ -30,6 +30,6 @@ source env/bin/activate
 
 echo "Start Testing"
 
-srun_if_on_slurm python3 src/test.py --num_of_goals=2 --max_steps=${max_steps} --test_runs=${test_runs} --output_path=${output_path} ${change_target} ${hard_reward}
+srun_if_on_slurm python3 src/test.py --num_of_goals=2 --max_steps=${max_steps} --test_runs=${test_runs} --output_path=${output_path} ${double} ${hard_reward}
 
 echo "Done Testing"
--- src/test.py
+++ src/test.py
@@ -11,14 +11,14 @@ if __name__=="__main__":
     
     parser=argparse.ArgumentParser()
     parser.add_argument('--output_path',type=str,default=os.path.join(os.getcwd(),'output'))
-    parser.add_argument('--change_target',action=argparse.BooleanOptionalAction,default=False)
+    parser.add_argument('--double',action=argparse.BooleanOptionalAction,default=False)
     parser.add_argument('--hard_reward',action=argparse.BooleanOptionalAction,default=False)
     parser.add_argument('--max_steps',type=int,default=10)
     parser.add_argument('--test_runs',type=int,default=1)
     parser.add_argument('--num_of_goals',type=int,default=3)
 
     args=parser.parse_args()
-    change_target=args.change_target
+    double=args.double
     hard_reward=args.hard_reward
     output_path=args.output_path
     max_steps=args.max_steps
@@ -28,13 +28,13 @@ if __name__=="__main__":
 
     model_path=os.path.join(output_path,'model')
 
-    environment=make_vec_env(MyReacherEnv,n_envs=n_envs,vec_env_cls=SubprocVecEnv,env_kwargs={'max_steps':max_steps,'output_path':output_path,'change_target':change_target,'hard_reward':hard_reward})
+    environment=make_vec_env(MyReacherEnv,n_envs=n_envs,vec_env_cls=SubprocVecEnv,env_kwargs={'max_steps':max_steps,'output_path':output_path,'double':double,'hard_reward':hard_reward})
         
     model=PPO.load(model_path,environment)
 
     trainer=Trainer(environment,model,output_path)
 
-    if change_target:
+    if double:
         trainer.test_changing_target(test_runs=test_runs)
     else:
         trainer.test_single_target(test_runs=test_runs)
