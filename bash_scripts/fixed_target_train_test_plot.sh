ee0fb4b6eae560f0c79a8ed3fe06e57f01ec8a2c
AlessandroCesaTs <alessandro.cesa22@gmail.com>
2024-10-13 18:10:41 +0200

    not testing

--- bash_scripts/fixed_target_train_test_plot.sh
+++ bash_scripts/fixed_target_train_test_plot.sh
@@ -7,9 +7,9 @@ source bash_scripts/slurm_utils.sh  #get slurm functions
 if i_am_on_slurm; then
     #I'm on slurm
     train_job_id=$(sbatch --cpus-per-task=1 --output=$(pwd)/outputs/${output_directory}/train/train.out bash_scripts/train.sh '--no-double' '--no-hard_reward' ${output_directory} | awk '{print $4}')
-    test_job_id=$(sbatch --cpus-per-task=1 --dependency=afterok:$train_job_id --output=$(pwd)/outputs/${output_directory}/test/test.out bash_scripts/test.sh '--no-double' '--no-hard_reward' ${output_directory} | awk '{print $4}')
-    plot_job_id=$(sbatch --dependency=afterok:$test_job_id --output=$(pwd)/outputs/${output_directory}/train/plot.out bash_scripts/plot.sh '--no-double' '--plot_test' ${output_directory}| awk '{print $4}')
-    echo "Submitted jobs $train_job_id, $test_job_id,$plot_job_id"
+    #test_job_id=$(sbatch --cpus-per-task=1 --dependency=afterok:$train_job_id --output=$(pwd)/outputs/${output_directory}/test/test.out bash_scripts/test.sh '--no-double' '--no-hard_reward' ${output_directory} | awk '{print $4}')
+    plot_job_id=$(sbatch --dependency=afterok:$train_job_id --output=$(pwd)/outputs/${output_directory}/train/plot.out bash_scripts/plot.sh '--no-double' '--no-plot_test' ${output_directory}| awk '{print $4}')
+    echo "Submitted jobs $train_job_id,,$plot_job_id"
 else
     bash_scripts/train.sh '--no-double'
     bash_scripts/test.sh '--no-double'
